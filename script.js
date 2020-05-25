829466884e7c579c7b9d3af698de1bf94f1ed041
Tomaz <tmedrado99@gmail.com>
2020-05-25 19:05:31 -0300

    Assez Bonne

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//Code pour la premiere fonctionalité
 var footerClique = document.getElementById("footer")
 var countClick = 1
 function sayHi(){
@@ -6,10 +7,49 @@ function sayHi(){
 }
 footerClique.addEventListener("click", sayHi);
 
-
-var hambBtn  = document.getElementById("navbarHeader")
-function uncollapse(){
-    className = "bg-dark";
-    console.log("Working!");
+//Code pour la deuxieme fonctionalite
+var hambBtn = document.getElementById("navbar-toggler")
+var navbarHeader = document.getElementById("navbarHeader")
+function changeClass(){
+    if (navbarHeader.className === "collapse bg-dark"){
+    navbarHeader.className = "bg-dark"
+    }
+    else {
+        navbarHeader.className = "collapse bg-dark"
+        }
 }
-hambBtn.addEventListener("click", uncollapse)
+
+hambBtn.addEventListener("click", changeClass);
+
+//Code pour la troisieme fonctionalite
+var firstCardEditButton = document.getElementById("firstEditButton")
+var firstCardText = document.getElementById("firstCardText")
+function changeColor(){
+    firstCardText.style = "color: red";
+}
+
+firstCardEditButton.addEventListener("click",  changeColor)
+
+//Code pour la quatrième fonctionalite
+var secondCardButton = document.getElementById("secondEditButton")
+var secondCardText = document.getElementById("secondCardText")
+
+function changeColor2(){
+    if (secondCardText.style.color ==='green'){
+    secondCardText.style.color =''
+    }
+    else {
+        secondCardText.style.color ='green'
+        }
+}
+
+secondCardButton.addEventListener("click",  changeColor2)
+
+//Code pour la cinquième fonctionalite
+var bootstrapLink = document.getElementById("bootstrapLink")
+
+function unableBS(){
+    bootstrapLink.setAttribute('href', '');
+}
+
+navbarHeader.addEventListener("dblclick", unableBS)
