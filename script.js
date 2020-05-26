62520ae19deea668dd1b8730fca677b9c520c8b7
Tomaz <tmedrado99@gmail.com>
2020-05-25 21:33:01 -0300

    Final Commit

--- script.js
+++ script.js
@@ -53,3 +53,36 @@ function unableBS(){
 }
 
 navbarHeader.addEventListener("dblclick", unableBS)
+
+
+//Code pour la sexième fonctionalite
+//testEditButton.addEventListener('mouseover', changeCards)
+
+var CardEditButtom = document.getElementsByClassName('btn-group')
+var CardText = document.getElementsByClassName('card-text')
+var CardImage = document.getElementsByClassName('card-img-top')
+
+for (let counter = 0; counter < 6; counter++){
+    CardEditButtom[counter].addEventListener('mouseover', function(){
+        CardText[counter].innerText = ""
+        CardImage[counter].style = "width:20%"
+})}
+
+
+//Code pour la septieme fonctionalite
+var arrowButton = document.getElementsByClassName('btn btn-secondary my-2')[0]
+var auxVar1 = CardImage[5]
+var auxVar2 = CardText[5].innerText
+
+
+//arrowButton.addEventListener('click', function(){
+//    CardText[5].innerText = CardText[0].innerText;
+//    CardImage[5].src = CardImage[0].src;
+//})
+
+
+//for (let counter = 0; counter < 6; counter++){
+  //CardText[counter].innerText = CardText[counter+1].innerText;
+ // CardImage[counter] = CardImage[counter+1]
+  
+//}
